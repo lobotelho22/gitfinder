afc081b4ca4b2fa064920d0121411b4d9ac086e3
lobotelho22 <eduardo.ocara@gmail.com>
2023-12-29 16:02:13 -0300

    dev: criando secoes da aplicacao

---
+++ src/components/itemlist/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const ItemList = ({title, description}) => {
+  return (
+    <article>
+        <strong>Title</strong>
+        <p>description</p>
+        <hr />
+    </article>
+  )
+}
+
+export default ItemList
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,21 @@
 import Header from "../../components/header";
+import ItemList from "../../components/itemlist";
 import background from "../../assets/background.png";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+
       <Header />
+
       <div className="contents">
-        <img src={ background}  alt="Github logo" className="background"/>
+        
+        <img src={background} alt="Github logo" className="background" />
+        
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"/>
+            <input name="usuario" placeholder="@username" />
             <button>Buscar</button>
           </div>
           <div className="profile">
@@ -21,9 +26,18 @@ function App() {
             />
             <div>
               <h3>Nome do Usuário</h3>
+              <span>@usuario</span>
               <p>Descrição</p>
             </div>
           </div>
+        
+          <hr />
+        
+          <div className="itemList">
+            <h3>Repositórios</h3>
+            <ItemList />
+          </div>
+        
         </div>
       </div>
     </div>
